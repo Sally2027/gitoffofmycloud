fdaeffdf3202bbc856b81b9254181ce076b3c01b
Sally2027 <sallyalazab123@gmail.com>
2025-04-24 18:55:54 +0000

    working on the project

--- project/assets/js/slick.js
+++ project/assets/js/slick.js
@@ -19,3 +19,18 @@ $(window).on('load', function() {
     });
   });
   
+  $(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
